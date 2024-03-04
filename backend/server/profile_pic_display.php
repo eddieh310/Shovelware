e8aaabbacda7a58e70332bd8c0a9c4b6d3a54f95
eddieh310 <ejherrer@buffalo.edu>
2024-03-03 22:38:00 -0500

    #43 on Trello, updated file for uploading profile picture, and added file for displaying profile picure

---
+++ backend/server/profile_pic_display.php
@@ -0,0 +1,49 @@
+<?php
+
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Header: Content-Type');
+
+if (isset($_COOKIE["user_email"])){
+    $userEmail = $_COOKIE["user_email"];
+
+    $userEmail = filter_var($userEmail, FILTER_SANITIZE_EMAIL);
+
+    $servername = "oceanus.cse.buffalo.edu";
+    $username = "ejherrer";
+    $password = "50394558";
+    $dbname = "cse442_2024_spring_team_ae_db";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT filename FROM profile_pictures WHERE email = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $userEmail);
+
+    $stmt->execute();
+
+    // Get entry in sql table  
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $filename = $row["filename"];
+
+        // Send filename to frontend if it exists
+        echo json_encode(array("filename" => $filename));
+    } else {
+        echo "No profile picture found for this user.";
+    }
+    $conn->close();
+    $stmt->close();
+} else {
+    // User email cookie not set
+    echo "User email cookie not set.";
+}
+
+
---
+++ backend/server/profile_pic_upload.php
@@ -0,0 +1,45 @@
+<?php
+
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Header: Content-Type');
+
+    $servername = "oceanus.cse.buffalo.edu";
+    $username = "ejherrer";
+    $password = "50394558";
+    $dbname = "cse442_2024_spring_team_ae_db";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "CREATE TABLE IF NOT EXISTS profile_pictures (
+        id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        file_name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+        )";
+
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    $file_name = $data["file_name"];
+    $email = $data["email"];
+
+    $query = "INSERT INTO profile_pic (file_name, email) VALUES (?, ?)";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("ss", $file_name, $email);
+    $stmt->execute();
+
+    if ($stmt->affected_rows > 0) {
+        echo "Data inserted successfully.";
+    } else {
+        echo "Error inserting data: " . $conn->error;
+    }
+
+    $stmt->close();
+
+    mysqli_close($conn);
+
+?>
